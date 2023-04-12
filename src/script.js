3af244513710ffc6d39b9ec2697381a5df4ac32a
MirakoandMiracle <118588409+MirakoandMiracle@users.noreply.github.com>
2023-04-12 16:14:05 +0630

    unit change and current location

--- src/script.js
+++ src/script.js
@@ -26,16 +26,18 @@ function DisplayTemp(response) {
   let city = document.querySelector("#city");
   let country = document.querySelector(".country");
   let date = document.querySelector("#date");
-  let weathercondition = document.querySelector("#weather-condition");
-  let icon = document.querySelector(".icon");
   let Tempture = document.querySelector(".temp");
   let unitForT = document.querySelector(".unitT");
+  let weathercondition = document.querySelector("#weather-condition");
+  let icon = document.querySelector(".icon");
   let wind = document.querySelector(".wind");
   let humidity = document.querySelector(".humidity");
   country.innerHTML = response.data.country;
   city.innerHTML = response.data.city;
   weathercondition.innerHTML = response.data.condition.description;
-  Tempture.innerHTML = Math.round(response.data.temperature.current);
+  unitForT.innerHTML = "°C";
+  celsiustemperature = Math.round(response.data.temperature.current);
+  Tempture.innerHTML = celsiustemperature;
   icon.setAttribute("src", response.data.condition.icon_url);
   icon.setAttribute("alt", response.data.condition.icon);
   date.innerHTML = formatDate(response.data.time * 1000);
@@ -43,9 +45,9 @@ function DisplayTemp(response) {
   humidity.innerHTML = `${response.data.temperature.humidity}% `;
 }
 
-function SearchcityWeather(city) {
+function SearchcityWeather(city, lon, lat) {
   let Apikey = "b3584c6545a2013b0440f785b9e39t5o";
-  let Url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${Apikey}&units=metric`;
+  let Url = `https://api.shecodes.io/weather/v1/current?query=${city}&lon=${lon}&lat=${lat}&key=${Apikey}&units=metric`;
   axios.get(Url).then(DisplayTemp);
 }
 
@@ -56,3 +58,39 @@ function getCity(event) {
 }
 let Search = document.querySelector(".SearchforCity");
 Search.addEventListener("submit", getCity);
+
+function Celsiusahange(event) {
+  event.preventDefault();
+  let Tempture = document.querySelector(".temp");
+  let unitForT = document.querySelector(".unitT");
+  Tempture.innerHTML = celsiustemperature;
+  unitForT.innerHTML = "°C";
+}
+let celsiustemperature = null;
+
+let Cbtn = document.querySelector(".c");
+Cbtn.addEventListener("click", Celsiusahange);
+
+function FarenChange(event) {
+  event.preventDefault();
+  let Tempture = document.querySelector(".temp");
+  let unitForT = document.querySelector(".unitT");
+  let fahrenheiTemperature = (celsiustemperature * 9) / 5 + 32;
+  Tempture.innerHTML = Math.round(fahrenheiTemperature);
+  unitForT.innerHTML = "°F";
+}
+
+let Fbtn = document.querySelector(".f");
+Fbtn.addEventListener("click", FarenChange);
+
+function getLocation(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let Apikey = "b3584c6545a2013b0440f785b9e39t5o";
+  let Url = `https://api.shecodes.io/weather/v1/current?query=${city}&lon=${lon}&lat=${lat}&key=${Apikey}&units=metric`;
+  axios.get(Url).then(DisplayTemp);
+}
+
+navigator.geolocation.getCurrentPosition(getLocation);
+
+SearchcityWeather(getLocation);
